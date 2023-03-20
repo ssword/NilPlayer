e1c5d634faafb51a0a24afa79c53ce20769e0d6c
ssword <ssword@gmail.com>
2023-03-20 23:59:28 +0800

    using a different approach for nil player

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,7 @@
-extern crate gtk;
-
-use std::env;
-use gtk::glib;
-use gtk::prelude::*;
-use gtk::Builder;
-use gtk::{
-    Toolbar,
-    ToolButton,
-};
-
-
-const PLAY_STOCK: &str = "gtk-media-play";
-
-fn main() {
-
-    let application = gtk::Application::new(Some("com.github.ssword.nilplayer"), Default::default());
-    application.connect_activate(|application| {
-        let window = gtk::ApplicationWindow::new(application);
-        window.set_title("NilPlayer");
-
-        let toolbar = Toolbar::new();
-        window.add(&toolbar);
-
-        let open_button = ToolButton::new_from_stock("gtk-open");
-        toolbar.add(&open_button);
-        window.show();
-    });
-    application.run();
-}
+use nil_utils::decode::Decoder;
+use nil_utils::errors::NilError;
+use nil_wav::decode::WavDecoder;
+use rodio::{buffer::SampleBuffer, OutputStream};
+use std::path::Path;
+use std::thread;
+use std::time::Duration;
